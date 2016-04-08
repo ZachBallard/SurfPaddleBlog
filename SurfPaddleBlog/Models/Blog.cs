6f8ce14ec0cd4da4bd7340d0180ac772eae709d5
Zach Ballard <zbginji@gmail.com>
2016-04-08 12:30:52 -0500

    building surf&paddle original into mvc

---
+++ SurfPaddleBlog/Models/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurfPaddleBlog.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
+        public string BigMiddleText { get; set; }
+        public string AuthorPictureUrl { get; set; }
+        public string AdPictureUrl { get; set; }
+        public string SideBarTitle { get; set; }
+        public string SideBarBody { get; set; }
+    }
+}
---
+++ SurfPaddleBlog/Models/SurfPaddleBlogContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SurfPaddleBlog.Models
+{
+    public class SurfPaddleBlogContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public SurfPaddleBlogContext() : base("name=SurfPaddleBlogContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<SurfPaddleBlog.Models.Blog> Blogs { get; set; }
+    }
+}
